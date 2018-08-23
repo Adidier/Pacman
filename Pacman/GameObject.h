c544e1b1435bce64d6c9e560f8eed1a49024aab9
adidier <adidier.perez@gmail.com>
2018-08-23 08:10:59 -0500

    first commit

---
+++ Pacman/GameObject.h
@@ -0,0 +1,28 @@
+#pragma once
+
+enum Type {
+	wall,
+	pellet,
+	ghost,
+	cherry,
+	pacman,
+	superPellet
+};
+
+class GameObject
+{
+protected:
+	float x, y;
+	Type type;
+public:
+	void GetPosition(float &x, float &y);
+	void SetPosition(float x, float y);
+	void SetType(Type t);
+	Type GetType();
+	GameObject();
+	~GameObject();
+	int GetPositionX();
+	void SetPositionX(float x);
+	int GetPositionY();
+	void SetPositionY(float y);
+};
